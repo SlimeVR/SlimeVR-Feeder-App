cb204ab413bad31ae9d2b60ab8aad60ed21881df
Sapphire <imsapphire0@gmail.com>
2026-02-01 20:05:18 -0600

    Add feet mounting reset action

--- src/main.cpp
+++ src/main.cpp
@@ -791,6 +791,7 @@ int main(int argc, char* argv[]) {
 	VRActionHandle_t calibration_action = GetAction("/actions/main/in/request_calibration");
 	VRActionHandle_t fast_reset_action = GetAction("/actions/main/in/fast_reset");
 	VRActionHandle_t mounting_reset_action = GetAction("/actions/main/in/mounting_reset");
+	VRActionHandle_t feet_mounting_reset_action = GetAction("/actions/main/in/feet_mounting_reset");
 	VRActionHandle_t pause_tracking_action = GetAction("/actions/main/in/pause_tracking");
 
 	//trackers.Detect(false);
@@ -851,6 +852,7 @@ int main(int argc, char* argv[]) {
 		trackers.HandleDigitalActionBool(calibration_action, { "reset" });
 		trackers.HandleDigitalActionBool(fast_reset_action, { "fast_reset" });
 		trackers.HandleDigitalActionBool(mounting_reset_action, { "mounting_reset" });
+		trackers.HandleDigitalActionBool(feet_mounting_reset_action, { "feet_mounting_reset" });
 		trackers.HandleDigitalActionBool(pause_tracking_action, { "pause_tracking" });
 
 		trackers.Tick(just_connected);
